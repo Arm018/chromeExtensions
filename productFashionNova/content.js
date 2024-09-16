8d3b2b06d801c87d9383e2c19b10ae136fd192ab
Arm018 <arm0182626@gmail.com>
2024-09-16 16:27:00 +0400

    FashionNova title color change

---
+++ productFashionNova/content.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    const productTitle = document.querySelector('.product-info__title');
+
+    if (productTitle) {
+        productTitle.style.color = 'red';
+    }
+});
