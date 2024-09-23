5e5312cd5974355f25f15093ce426cf6a9c1a4bb
Arm018 <arm0182626@gmail.com>
2024-09-23 13:52:40 +0400

    Adding laravel api for react

---
+++ fashionNova-api/app/Http/Api/ProductController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\Http\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ProductRequest;
+use App\Models\Product;
+use Illuminate\Http\JsonResponse;
+
+class ProductController extends Controller
+{
+    public function save(ProductRequest $request): JsonResponse
+    {
+        $product = new Product();
+        $product->fill($request->validated());
+        $product->save();
+        return response()->json($product);
+    }
+}
